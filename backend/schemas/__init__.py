1f9bf2fd72386503c2f6c6181bf19d73e33156e4
atremante26 <atremante26@amherst.edu>
2026-02-24 19:18:47 -0500

    Updated schemas __init__.py

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -1,8 +1,9 @@
 from .clustering import ClusterResponse
-from .forecasting import ForecastsResponse, HistoricalResponse
+from .forecasting import ForecastsResponse, HistoricalPoint, HistoricalResponse
 
 __all__ = [
     "ClusterResponse",
     "ForecastsResponse",
+    "HistoricalPoint",
     "HistoricalResponse"
 ]
