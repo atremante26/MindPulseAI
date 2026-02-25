6ef842f0ef15d910121c78313582f86cf2296fd1
atremante26 <atremante26@amherst.edu>
2026-02-24 19:34:28 -0500

    Fixed method names

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -2,7 +2,7 @@ from .clustering_service import load_clustering_data
 from .forecasting_service import load_forecasting_data, get_historical
 
 __all__ = [
-    "load_data",
-    "load_data",
+    "load_clustering_data",
+    "load_forecasting_data",
     "get_historical"
 ]
