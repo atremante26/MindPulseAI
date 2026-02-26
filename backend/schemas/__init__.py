c928767959508af6ecc6374d75656ce8fc3455b7
atremante26 <atremante26@amherst.edu>
2026-02-26 01:03:11 -0500

    Added recommendation schemas to __init__.py

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -1,6 +1,7 @@
 from .clustering import ClusterResponse
 from .forecasting import ForecastsResponse, HistoricalPoint, HistoricalResponse
 from .insights import WeeklyInsightsResponse, DatapointRequest, DatapointResponse
+from .recommendations import RecommendationRequest, RecommendationResponse
 
 __all__ = [
     "ClusterResponse",
@@ -9,5 +10,7 @@ __all__ = [
     "HistoricalResponse",
     "WeeklyInsightsResponse", 
     "DatapointRequest", 
-    "DatapointResponse"
+    "DatapointResponse",
+    "RecommendationRequest",
+    "RecommendationResponse"
 ]
